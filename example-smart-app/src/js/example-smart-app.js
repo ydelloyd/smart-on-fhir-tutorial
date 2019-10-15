c71586c0872a8bc516e0b3cca7bf0ea56842bdfe
yd837d <yd837d@att.com>
2019-10-15 16:16:44 -0400

    added more weights

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -37,7 +37,7 @@
           }
 
           var height = byCodes('8302-2');
-          var weight = byCodes('29463-7');
+          var weight = byCodes('3141-9');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -48,6 +48,9 @@
           p.gender = gender;
           p.fname = fname;
           p.lname = lname;
+          console.log(p.weight);
+          weight = byCodes('29463-7');
+          console.log(p.weight);
           p.weight = getQuantityValueAndUnit(weight[0]);
           p.height = getQuantityValueAndUnit(height[0]);
 
